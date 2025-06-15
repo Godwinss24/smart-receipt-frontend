11d0e5f7c238ecbe1c1d3837f820c73dd2c8bc00
Godwinss24 <godwinsudoinyang@gmail.com>
2025-06-15 12:25:23 +0100

    added-linking-and-responsiveness

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 px-6 sm:px-6 lg:px-8">
       <!-- Header with Logo -->
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
         <div class="flex justify-center">
